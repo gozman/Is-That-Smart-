3c2847c2567af080c826ce8607eded9c398b62b2
Mike Gozzo <mgozzo@Gozbook.local>
2012-02-24 23:41:56 -0500

    added phil's twittermonitor

---
+++ TwitterMonitor/lib/listeners/twitter.js
@@ -0,0 +1,24 @@
+/*
+ *
+ * listeners/twitter.js - Event listeners for Hook.io-Twitter.
+ *
+ * (c) 2011 Nodejitsu Inc.
+ *
+ */
+
+var twitter = module.exports = function () {
+  var self = this;
+
+  self.on('*::keptTweet', function (data) {
+    // self.sayTweet('data);
+    console.log(data);
+  });
+
+  self.on('*::reported', function (data) {
+    self.emit('sendMsg', {dest: data.to, msg: 'I have reported ' + data.name + ' as a spammer.'});
+  });
+
+  self.on('*::blocked', function (data) {
+    self.emit('sendMsg', {dest: data.to, msg: 'I have blocked ' + data.name + '.'});
+  });
+};
